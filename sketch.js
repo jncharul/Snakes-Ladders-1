dfd90a1e9c9a83584bb65503fe1bf2fe8765fc49
jncharul <74925396+jncharul@users.noreply.github.com>
2021-05-18 22:29:00 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -15,79 +15,168 @@ function setup(){
 }
 
 function draw(){
-    background("red");
-    for(var i = 0;i<100;i++){
-        
-        boxes[i].display();
-        // textSize(20);
-        // fill("black")
-        // text(i+1,boxes[i].x+20,boxes[i].y+40)
-        
-    }
-    textSize(20);
-        fill("black")
+    background("white");
+      
+ 
     for(var i = 0;i<10;i++){
-        
+        if(i%2==0){
+            fill("yellow")
+        }
+        else{
+            fill("red");
+        }
+        boxes[i].display();
+        textSize(20);
+        fill("black")
         text(i+1,boxes[i].x+20,boxes[i].y+40)
 
+    }
+    for(var i = 10;i<20;i++){
+        if(i%2!==0){
+                    fill("yellow")
+                }
+                else{
+                    fill("red");
+                }
+                boxes[i].display();
     }
     var display_x = width-45;
     for(var i = 10;i<20;i++){
         
         var display_y = height -100;
+        textSize(20);
+        fill("black")
         text(i+1,display_x,display_y);
         display_x -= 73;
     }
     for(var i = 20;i<30;i++){
-        
+        if(i%2==0){
+            fill("yellow")
+        }
+        else{
+            fill("red");
+        }
+        boxes[i].display();
+        textSize(20);
+        fill("black")
         text(i+1,boxes[i].x+20,boxes[i].y+40)
 
     }
+    for(var i = 30;i<40;i++){
+    if(i%2!==0){
+        fill("yellow")
+    }
+    else{
+        fill("red");
+    }
+    boxes[i].display();
+}
     var display_x = width-45;
     for(var i = 30;i<40;i++){
         
+        textSize(20);
+        fill("black")
         var display_y = height -235;
         text(i+1,display_x,display_y);
         display_x -= 73;
     }
     for(var i = 40;i<50;i++){
-        
+        if(i%2==0){
+            fill("yellow")
+        }
+        else{
+            fill("red");
+        }
+        boxes[i].display();
+        textSize(20);
+        fill("black")
         text(i+1,boxes[i].x+20,boxes[i].y+40)
 
+    }
+    for(var i = 50;i<60;i++){
+        if(i%2!==0){
+                    fill("yellow")
+                }
+                else{
+                    fill("red");
+                }
+                boxes[i].display();
     }
     var display_x = width-45;
     for(var i = 50;i<60;i++){
         
         var display_y = height -370;
+        textSize(20);
+        fill("black")
         text(i+1,display_x,display_y);
         display_x -= 73;
     }
     for(var i = 60;i<70;i++){
-        
+        if(i%2==0){
+            fill("yellow")
+        }
+        else{
+            fill("red");
+        }
+        boxes[i].display();
+        textSize(20);
+        fill("black")
         text(i+1,boxes[i].x+20,boxes[i].y+40)
 
+    }
+    for(var i = 70;i<80;i++){
+        if(i%2!==0){
+                    fill("yellow")
+                }
+                else{
+                    fill("red");
+                }
+                boxes[i].display();
     }
     var display_x = width-45;
     for(var i = 70;i<80;i++){
         
         var display_y = height -520;
+        textSize(20);
+        fill("black")
         text(i+1,display_x,display_y);
         display_x -= 73;
     }
     for(var i = 80;i<90;i++){
-        
+        if(i%2==0){
+            fill("yellow")
+        }
+        else{
+            fill("red");
+        }
+        boxes[i].display();
+        textSize(20);
+        fill("black")
         text(i+1,boxes[i].x+20,boxes[i].y+40)
 
+    }
+   
+
+    for(var i = 90;i<100;i++){
+        if(i%2!==0){
+                    fill("yellow")
+                }
+                else{
+                    fill("red");
+                }
+                boxes[i].display();
     }
     var display_x = width-45;
     for(var i = 90;i<100;i++){
         
         var display_y = height -670;
+        fill("black")
         text(i+1,display_x,display_y);
         display_x -= 73;
+        console.log(i)
     }
         
-        //console.log(i);
+        
         
     
 }
